797125729f92a4fd6880a88572191a82d39eb65e
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-03-09 17:36:49 +0800

    trying to implement api post request

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,9 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, render_template, redirect
+from models.user import User
+
 
 users_api_blueprint = Blueprint('users_api',
-                             __name__,
-                             template_folder='templates')
+                                __name__,
+                                template_folder='templates')
+
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
-    return "USERS API"
+    users = User.select()
+    user_list = []
+    for user in users:
+        data = user.__dict__
+        user_list.append(data['__data__'])
+    return jsonify(user_list)
+
+
+@users_api_blueprint.route('/create', methods=['POST'])
+def create():
+    resp = request.get('http://localhost: 5000/api/v1/users/create')
+    return render_template('new.html', response=resp.json)
--- models/user.py
+++ models/user.py
@@ -4,3 +4,16 @@ import peewee as pw
 
 class User(BaseModel):
     name = pw.CharField(unique=False)
+    password = pw.CharField(unique=False)
+    email = pw.CharField(unique=True)
+    address = pw.TextField(unique=False, null=True)
+    blood_group = pw.CharField(unique=False, null=True)
+
+    def validate(self):
+        email_taken = User.get_or_none(self.email == User.email)
+        if email_taken:
+            self.errors.append('That email is taken.')
+
+        if self.blood_group not in ['A pos.', 'B pos.', 'O pos.', 'AB pos.', 'A neg.', 'B neg.', 'O neg.', 'AB neg.']:
+            self.errors.append(
+                'Blood group only accepts this format (A neg. | O pos.)')
