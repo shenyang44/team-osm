f7c131e541179432c35ae8a2d9f606ced2635e78
unknown <110853MY@gmail.com>
2020-03-10 18:13:23 +0800

    done establishment/events/bloodinv API

--- instagram_api/__init__.py
+++ instagram_api/__init__.py
@@ -1,7 +1,10 @@
 from instagram_api.blueprints.users.views import users_api_blueprint
+from instagram_api.blueprints.establishments.views import establishments_api_blueprint
 from app import app
 from flask_cors import CORS
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
+app.register_blueprint(establishments_api_blueprint,
+                       url_prefix='/api/v1/establishment')
