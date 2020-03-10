279ccbe3e630c4eae42016c764d5fd001b3054ba
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-03-10 13:46:52 +0800

    jwt

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify, request, render_template, redirect, json
 from models.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from playhouse.shortcuts import model_to_dict
+
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 
 users_api_blueprint = Blueprint('users_api',
                                 __name__,
@@ -12,8 +15,8 @@ def index():
     users = User.select()
     user_list = []
     for user in users:
-        data = user.__dict__
-        user_list.append(data['__data__'])
+        user = model_to_dict(user)
+        user_list.append(user)
     return jsonify(user_list)
 
 
@@ -24,11 +27,49 @@ def create():
     password = resp.get('password')
     email = resp.get('email')
     number = resp.get('number')
+    address = resp.get('address')
     hashed_pa = generate_password_hash(password)
-    user = User(name=name, password=hashed_pa, email=email, number=number)
+    user = User(name=name, password=hashed_pa,
+                email=email, number=number, address=address)
     if user.save():
+        user = User.get_or_none(User.email == email)
+        access_token = create_access_token(identity=user.id)
         return jsonify({
-            "message": 'fudge you, it worked'
+            "message": 'fudge you, it worked',
+            "access_token": access_token
         })
     else:
         return 'no'
+
+
+@users_api_blueprint.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    if not email:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    user = User.get_or_none(User.email == email)
+    # if username != 'test' or password != 'test':
+    #     return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify(access_token=access_token), 200
+
+
+# Protect a view with jwt_required, which requires a valid access token
+# in the request to access.
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+if __name__ == '__main__':
+    app.run()
