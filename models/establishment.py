9380cd2111ad4fd3dcead893f807710cc8be4c81
unknown <110853MY@gmail.com>
2020-03-12 11:55:22 +0800

    fishcake

--- models/establishment.py
+++ models/establishment.py
@@ -5,6 +5,7 @@ import peewee as pw
 class Establishment(BaseModel):
     name = pw.CharField(unique=False)
     address = pw.CharField(unique=False)
+    hospital_type = pw.CharField(unique=False, null=True)
 
     def validate(self):
         address_taken = Establishment.get_or_none(
